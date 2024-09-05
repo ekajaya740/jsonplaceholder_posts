b2fdb1881b0f3614a5c1bd986090937ba4168a32
ekajaya740 <ekajaya740@gmail.com>
2024-09-05 21:38:16 +0800

    feat: add required pages

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,5 +1,11 @@
 import 'package:go_router/go_router.dart';
+import 'package:jsonplaceholder_posts/pages/home_page.dart';
+import 'package:jsonplaceholder_posts/pages/post/post_page.dart';
 
 final router = GoRouter(routes: [
-  
+  GoRoute(path: '/', builder: (context, state) => const HomePage()),
+  GoRoute(
+    path: '/post/:id',
+    builder: (context, state) => const PostPage(),
+  )
 ]);
