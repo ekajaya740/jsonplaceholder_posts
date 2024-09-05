f504607c35bfa5d96f2d17cd92f4b0047e201d71
ekajaya740 <ekajaya740@gmail.com>
2024-09-05 20:17:26 +0800

    feat: add dio config

---
+++ lib/config/dio.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+
+enum DioMethod { post, get, put, delete }
+
+class DioService {
+  DioService._singleton();
+
+  final dio =
+      Dio(BaseOptions(baseUrl: "https://jsonplaceholder.typicode.com/"));
+
+  Future<Response> request(
+    String endpoint,
+    DioMethod method, {
+    Map<String, dynamic>? param,
+    String? contentType,
+    formData,
+  }) async {
+    switch (method) {
+      case DioMethod.post:
+        return dio.post(
+          endpoint,
+          data: param ?? formData,
+        );
+      case DioMethod.get:
+        return dio.get(
+          endpoint,
+          queryParameters: param,
+        );
+      case DioMethod.put:
+        return dio.put(
+          endpoint,
+          data: param ?? formData,
+        );
+      case DioMethod.delete:
+        return dio.delete(
+          endpoint,
+          data: param ?? formData,
+        );
+      default:
+        return dio.post(
+          endpoint,
+          data: param ?? formData,
+        );
+    }
+  }
+}
