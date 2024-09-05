40d2d0a5281f3b2a2c30bbdcae0525865caebad7
ekajaya740 <ekajaya740@gmail.com>
2024-09-05 21:59:31 +0800

    feat: change to PostsList

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jsonplaceholder_posts/widgets/lists/posts_list.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +7,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text("Home"),
+      body: SafeArea(child: PostsList()),
     );
   }
 }
