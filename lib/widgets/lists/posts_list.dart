c8c3b978a61863911f8d05c8c73704a959d72fad
ekajaya740 <ekajaya740@gmail.com>
2024-09-06 14:33:16 +0800

    feat: add content to list

--- lib/widgets/lists/posts_list.dart
+++ lib/widgets/lists/posts_list.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:jsonplaceholder_posts/schemas/post/post.dart';
+import 'package:go_router/go_router.dart';
 
 class PostsList extends StatelessWidget {
-  const PostsList({super.key});
+  const PostsList({super.key, required this.posts});
+
+  final List<Post> posts;
 
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: (context, index) => ListTile(
+        title: Text(
+          posts.elementAt(index).title,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+        subtitle: Text(
+          posts.elementAt(index).body,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        onTap: () {
+          context.push('/post/${posts.elementAt(index).id}');
+        },
+      ),
+    );
   }
 }
