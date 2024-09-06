79e681d5efb05bb5dd90b056c3c85e17d7f206ac
ekajaya740 <ekajaya740@gmail.com>
2024-09-06 14:29:28 +0800

    feat: add router config

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:jsonplaceholder_posts/pages/home_page.dart';
 import 'package:jsonplaceholder_posts/pages/post/post_page.dart';
@@ -5,7 +6,12 @@ import 'package:jsonplaceholder_posts/pages/post/post_page.dart';
 final router = GoRouter(routes: [
   GoRoute(path: '/', builder: (context, state) => const HomePage()),
   GoRoute(
-    path: '/post/:id',
-    builder: (context, state) => const PostPage(),
-  )
+      path: '/post/:id',
+      builder: (context, state) {
+        final params = state.pathParameters['id'];
+        if (params == null) {
+          return const Text("No Parans");
+        }
+        return PostPage(id: int.parse(params));
+      })
 ]);
